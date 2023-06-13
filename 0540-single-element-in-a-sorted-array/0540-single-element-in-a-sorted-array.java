b2fcd5461028d0f31cbddea1717a7b492e0ac630
Aditya <96553801+adityaguggari@users.noreply.github.com>
2023-06-12 19:27:24 -0700

    Time: 0 ms (100.00%), Space: 50.1 MB (28.23%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,24 +3,22 @@ class Solution {
         int low=0;
         int high=nums.length-2;
         while(low<=high) {
-            int mid= low+ (high-low) / 2 ;
-            if (mid % 2 == 0) {
-                // Checking whether we are in right half
-                if (nums[mid] != nums[mid + 1]) 
-                    
-                    //Shrinking the right half
-                    high = mid - 1; 
-                else
-                    // Shrinking the left half
-                    low = mid + 1; 
-            } else {
-                // Checking whether we are in right half
-                if (nums[mid] == nums[mid + 1])
-                    //Shrinking the right half
-                    high = mid - 1; 
-                else
-                    // Shrinking the left half
-                    low = mid + 1; 
+            int mid=low+ (high-low) /2;
+            if(mid % 2 == 0) {
+                if(nums[mid]!=nums[mid+1]) {
+                    high=mid-1;
+                }
+                else {
+                    low=mid+1;
+                }
+            }
+            else {
+                if(nums[mid]==nums[mid+1]) {
+                    high=mid-1;
+                }
+                else {
+                    low=mid+1;
+                }
             }
         }
         return nums[low];
