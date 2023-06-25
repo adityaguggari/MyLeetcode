3f848914094b12c445811a74a20315144e3ab92e
Aditya <96553801+adityaguggari@users.noreply.github.com>
2023-06-24 20:43:53 -0700

    Time: 1 ms (89.60%), Space: 41.4 MB (63.01%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -16,23 +16,23 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        
         if(root == null) return result;
         
-        Queue<TreeNode> queue=new LinkedList<>();
+        Queue<TreeNode> queue= new LinkedList<>();
         queue.add(root);
-        boolean LefttoRight = true;
+        boolean LeftToRight = true;
         while(!queue.isEmpty()) {
             int size = queue.size();
             List<Integer> currentlevel = new ArrayList<>();
-            for(int i=0;i<size;i++) {
+            for(int i = 0; i < size ; i++) {
                 TreeNode current = queue.remove();
-                if(LefttoRight==true) {
+                if(LeftToRight == true) {
                     currentlevel.add(current.val);
                 }
                 else {
                     currentlevel.add(0, current.val);
                 }
+                
                 if(current.left!=null) {
                     queue.add(current.left);
                 }
@@ -40,7 +40,7 @@ class Solution {
                     queue.add(current.right);
                 }
             }
-            LefttoRight = !LefttoRight;
+            LeftToRight = !LeftToRight;
             result.add(currentlevel);
         }
         return result;
