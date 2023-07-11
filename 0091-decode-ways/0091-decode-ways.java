c68b668e507524d7411db0989b4f056db3d4f01a
Aditya <96553801+adityaguggari@users.noreply.github.com>
2023-07-10 23:58:36 -0700

    Time: 2 ms (29.18%), Space: 41.1 MB (29.67%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numDecodings(String s) {
-       // if(s.length() == 0) return 0;
+        if(s.length() == 0) return 0;
         int[] dp = new int[s.length()+1];
         dp[0] = 1;
         dp[1] = s.charAt(0) == '0' ? 0 : 1;
